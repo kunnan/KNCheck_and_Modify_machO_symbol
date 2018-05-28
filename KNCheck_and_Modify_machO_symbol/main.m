718587d0133060487096b8f2fee9f634af8f3d76
zhangkn <developerkunnan@gmail.com>
2018-05-28 14:50:11 +0800

    Initial Commit

---
+++ KNCheck_and_Modify_machO_symbol/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KNCheck_and_Modify_machO_symbol
+//
+//  Created by devzkn on 28/05/2018.
+//  Copyright © 2018 devzkn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
